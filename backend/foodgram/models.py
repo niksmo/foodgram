97881694bc4bae6c803b2b199b340e1d114ff01b
nikolay <n.smolov@yandex.ru>
2024-10-15 00:27:59 +0300

    start recipe model

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
 
 class Ingredient(models.Model):
     name = models.CharField('Название',
@@ -28,3 +30,49 @@ class Tag(models.Model):
 
     class Meta:
         pass
+
+
+class Recipe(models.Model):
+    name = models.CharField('Название',
+                            max_length=256)
+
+    text = models.TextField('Описание')
+
+    image = models.ImageField('Картинка',
+                              upload_to='recipes/')
+
+    cooking_time = models.SmallIntegerField('Время приголовления',
+                                            help_text='минут')
+
+    author = models.ForeignKey(User,
+                               related_name='recipes',
+                               on_delete=models.CASCADE)
+
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='RecipeIngredient')
+
+    tags = models.ManyToManyField(Tag,
+                                  through='TagRecipe')
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(name='positive_cooking_time',
+                                   check=models.Q(cooking_time__gt=0))
+        ]
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.SmallIntegerField('Количество')
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(name='positive_amount',
+                                   check=models.Q(amount__gt=0))
+        ]
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
