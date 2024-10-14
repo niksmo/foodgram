1db28fb3de1c14b2d542e57a66b69da3a82571cc
nikolay <n.smolov@yandex.ru>
2024-10-14 17:42:50 +0300

    adds Tag model

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -3,8 +3,29 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField('Название', max_length=128, unique=True)
-    measurement_unit = models.CharField('Единица измерения', max_length=64)
+    name = models.CharField('Название',
+                            max_length=128,
+                            unique=True,
+                            blank=False)
+
+    measurement_unit = models.CharField('Единица измерения',
+                                        max_length=64,
+                                        blank=False)
+
+    class Meta:
+        pass
+
+
+class Tag(models.Model):
+    name = models.CharField('Название',
+                            max_length=32,
+                            unique=True,
+                            blank=False)
+
+    slug = models.SlugField(max_length=32,
+                            unique=True,
+                            blank=True,
+                            null=True)
 
     class Meta:
         pass
