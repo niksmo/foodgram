20023b77b150eda34bcc3cceb45bb696a36aa431
nikolay <n.smolov@yandex.ru>
2024-10-25 15:26:24 +0300

    disable workflow, adds page pagination

--- backend/project_foodgram/settings.py
+++ backend/project_foodgram/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
 
